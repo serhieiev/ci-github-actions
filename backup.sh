9a2dc6da2cf7191ed42626a01637e73e777db42f
Max Serhieiev <serhieiev@gmail.com>
2023-10-02 10:15:36 +0200

    Test backup on push

--- backup.sh
+++ backup.sh
@@ -80,7 +80,7 @@ mv "$VERSIONS_FILE.tmp" $VERSIONS_FILE
 # Check if MAX_BACKUPS is set as a non negative integer
 if [ -n "$MAX_BACKUPS" ]; then
     if ! [[ "$MAX_BACKUPS" =~ ^[0-9]+$ ]]; then
-        echo "Error: MAX_BACKUPS should be a non-negative integer value."
+        echo "Error: MAX_BACKUPS should be a non negative integer value."
         exit 1
     fi
 fi
